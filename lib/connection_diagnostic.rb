1f41f8ff681fe64d19e162086362df518a22adc0
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2014-07-30 12:53:41 +0200

    Initial commit

---
+++ lib/connection_diagnostic.rb
@@ -0,0 +1,27 @@
+require 'client'
+
+class ConnectionDiagnostic
+  # perform_diagnostic
+  # disconnects the client
+  # tries to connect the client 3 times
+  # if unsuccessful raise Error
+  # if successful sends 'AT#UD' to client (diagnostic message)
+  # and receives from the client the diagnostic info
+  # this info should be accesible on the attribute info
+
+  attr_accessor :status
+
+  def initialize client
+  	@status = client.disconnect
+  end
+
+  def perform_diagnostic client
+  	counter = 1
+  	while @status == false
+			@status = client.connect
+			counter += 1
+			return @status = client.connect if counter == 3
+		end
+  end
+
+end
